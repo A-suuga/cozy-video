805968ba4abcae500a515b1df8ce6f2f3f6467a7
Arthur GAUCHER <arthur.gaucher@gmail.com>
2018-11-03 22:51:22 +0100

    Component succesfully requests a download ID when clicking a link

---
+++ src/components/VideoViewer/DownloadLink.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class DownloadLink extends Component {
+  constructor(props, context) {
+    super(props, context)
+    const { client } = context
+    if (!context.client) {
+      throw new Error(
+        'Should be used with client in context (use CozyProvider to set context)'
+      )
+    }
+
+    this.client = client
+    this.state = { dlLink: null }
+  }
+
+  componentDidMount() {
+    this.dlLink = this.client
+      .collection('io.cozy.files')
+      .getDownloadLinkById(this.props.fileId)
+      .then(resp => {
+        this.setState({
+          dlLink: resp
+        })
+      })
+  }
+
+  render() {
+    if (!this.state.dlLink) {
+      return (
+        <div>
+          <h3>Loading link...</h3>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <h3>Watching Video with dlLink {this.state.dlLink}</h3>
+      </div>
+    )
+  }
+}
+
+DownloadLink.contextTypes = {
+  client: PropTypes.object,
+  store: PropTypes.object
+}
+
+DownloadLink.propTypes = {
+  fileId: PropTypes.string
+}
--- src/components/VideoViewer/VideoViewer.jsx
+++ src/components/VideoViewer/VideoViewer.jsx
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
+import DownloadLink from './DownloadLink'
+// import { Query } from 'cozy-client'
 
-class VideoViewer extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Watching Video {this.props.match.params.manifestId} ...</h3>
-      </div>
-    )
-  }
+// export const VideoViewer = () => {
+//   const query = client =>
+//     client
+//       .collection('io.cozy.files')
+//       .getDownloadLinkById('57064943d15f2a69385dfee00000a878')
+//   return (
+//     <Query query={query}>
+//       {({ fetchStatus }) => (
+//         <div>
+//           {fetchStatus !== 'loaded' ? (
+//             <h1>Fetching manifest uri...</h1>
+//           ) : (
+//             <h3>Watching Video with fileId ...</h3>
+//           )}
+//         </div>
+//       )}
+//     </Query>
+//   )
+//
+
+export const VideoViewer = ({ match }) => {
+  return <DownloadLink fileId={match.params.manifestId} />
 }
 
 export default VideoViewer
