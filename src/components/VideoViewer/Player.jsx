021a5fa6b7fd0b0ce45c2c89e020d7ea9afb19fb
Arthur GAUCHER <arthur.gaucher@gmail.com>
2019-11-30 17:00:01 +0100

    Update app to conform to new shaka-player version
    This version uses the new shaka-player version built with the UI.
    Video controls are brand new and let the user directly control
    the behaviour of the player.

--- src/components/VideoViewer/Player.jsx
+++ src/components/VideoViewer/Player.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import shaka from 'shaka-player'
+import shaka from 'shaka-player/dist/shaka-player.ui'
+import 'shaka-player/dist/controls.css'
 
 class Player extends Component {
   constructor(props, context) {
     super(props, context)
     // TODO remove 'http:' properly
     this.manifestUri = props.manifestUri.slice(5)
+    // TODO remove 'https:' properly
+    // this.manifestUri = props.manifestUri.slice(6)
     const { client } = context
     if (!props.manifestUri) {
       throw new Error('Empty Manifest Uri')
@@ -34,33 +37,44 @@ class Player extends Component {
     const token = this.client.getClient().token
     // TODO Remove 'http://' properly
     const uri = this.client.getClient().uri.slice(7)
+    // TODO Remove 'http://' properly
+    // const uri = this.client.getClient().uri.slice(8)
     const cozyInfo = {
       domain: uri,
       token: token.token
     }
+    let controls
     this.player = new shaka.Player(cozyInfo, this.video)
+    this.ui = new shaka.ui.Overlay(this.player, this.container, this.video)
     // Listen for error events.
-    this.player.addEventListener('error', this.onErrorEvent)
+    this.player.addEventListener('error', this.onPlayerErrorEvent)
+    controls = this.ui.getControls()
+    controls.addEventListener('error', this.onUIErrorEvent)
 
     // Player configuration
-    this.player.configure('preferredAudioLanguage', 'en')
-    this.player.configure('preferredTextLanguage', 'fr')
-    this.player.configure('streaming.alwaysStreamText', true)
+    // this.player.configure('preferredAudioLanguage', 'en')
+    // this.player.configure('preferredTextLanguage', 'fr')
+    // this.player.configure('streaming.alwaysStreamText', true)
 
     // Try to load a manifest.
     // This is an asynchronous process.
-    this.player.load(this.manifestUri).catch(this.onError) // onError is executed if the asynchronous load fails.
+    this.player.load(this.manifestUri).catch(this.onPlayerError) // onPlayerError is executed if the asynchronous load fails.
   }
 
-  onErrorEvent(event) {
+  onPlayerErrorEvent(errorEvent) {
     // Extract the shaka.util.Error object from the event.
-    this.onError(event.detail)
+    this.onError(errorEvent.detail)
   }
 
-  // onError(error) {
-  //   // Log the error.
-  //   console.error('Error code', error.code, 'object', error);
-  // }
+  onPlayerError(error) {
+    // Log the error.
+    // eslint-disable-next-line no-console
+    console.error('Error code', error.code, 'object', error)
+  }
+
+  onUIErrorEvent(errorEvent) {
+    this.onPlayerError(errorEvent.detail)
+  }
 
   componentWillUnmount() {
     // unmount stuff
@@ -73,13 +87,19 @@ class Player extends Component {
     // eslint-disable-next-line no-console
     console.warn('Player Manifest: ', this.manifestUri)
     return (
-      <video
+      <div
         ref={c => {
-          this.video = c
+          this.container = c
         }}
-        className="vid-player"
-        controls
-      />
+        style={{ maxWidth: 40 + 'em' }}
+      >
+        <video
+          ref={c => {
+            this.video = c
+          }}
+          style={{ width: 100 + '%', height: 100 + '%' }}
+        />
+      </div>
     )
   }
 }
